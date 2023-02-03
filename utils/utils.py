84fa7215cc50a9f074c8ebf93842d59596ed49a8
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-03 19:19:23 +0300

    add functions: sort_by_date(), get_last_transaction(), get_format_date()

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime
 
 def get_data(path):
     """
@@ -32,3 +32,30 @@ def filter_by_key(data: list[dict[str, str]],
         if key in transaction:
             filtered_data.append(transaction)
     return filtered_data
+
+
+def sort_by_date(data: list[dict[str, str]]) -> list[dict[str, str]]:
+    """
+    Сортирует по дате, от большей к меньшей
+    """
+    return sorted(data, key=lambda x: x['date'], reverse=True)
+
+
+def get_last_transaction(data: list[dict[str, str]],
+                         amount: int) -> list[dict[str, str]]:
+    """
+    Возвращает список последних операций в указанном количестве
+    """
+    if amount > 0:
+        if amount < len(data):
+            return data[:amount]
+        return data[:len(data)]
+    return []
+
+
+def get_format_date(transaction: dict[str, str]) -> str:
+    """
+    Возвращает дату в формате ДД.ММ.ГГГГ
+    """
+    date_format = datetime.fromisoformat(transaction["date"])
+    return date_format.strftime("%d.%m.%Y")
