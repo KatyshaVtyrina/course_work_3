79e62aa5b97bb21efc8b93a1bb033c9bbb19e1d2
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-03 19:22:12 +0300

    add functions: get_description(), get_info_from(), get_info_to(), get_hide_number, get_summ(), get_currency, get_result_transaction

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 
+
 def get_data(path):
     """
     Возвращает данные с внешнего ресурса в формате json
@@ -59,3 +60,84 @@ def get_format_date(transaction: dict[str, str]) -> str:
     """
     date_format = datetime.fromisoformat(transaction["date"])
     return date_format.strftime("%d.%m.%Y")
+
+
+def get_description(data: dict[str, str]) -> str:
+    """
+    Возвращает описание типа перевода
+    """
+    return data["description"]
+
+
+def get_info_from(transaction: dict[str, str]) -> str:
+    """
+    Получает информацию о получателе в замаскированном виде
+    """
+    words = ""
+    numbers = ""
+    for symbol in transaction["from"]:
+        if not symbol.isdigit():
+            words += symbol
+        else:
+            numbers += symbol
+    return f"{words}{get_hide_number(numbers)}"
+
+
+def get_info_to(transaction: dict[str, str]) -> str:
+    """
+    Получает информацию о получателе в замаскированном виде
+    """
+    words = ""
+    numbers = ""
+    for symbol in transaction["to"]:
+        if not symbol.isdigit():
+            words += symbol
+        else:
+            numbers += symbol
+    return f"{words}{get_hide_number(numbers)}"
+
+
+def get_hide_number(numbers: str) -> str:
+    """
+    Маскирует номер счета или карты
+    :param numbers: номер счета или карты (str)
+    :return: Возвращает номер карты в формате XXXX XX** **** XXXX или
+    номер счета в формате в формате **XXXX
+    """
+    hide_number = ""
+    for numb in numbers:
+        if numb.isdigit():
+            hide_number += numb
+    if len(numbers) == 16:
+        return numbers[:4] + " " + numbers[4:6] + "** **** " + numbers[12:]
+    elif len(numbers) == 20:
+        return "**" + numbers[16:]
+
+
+def get_summ(transaction: dict[str, dict[str, str]]) -> str:
+    """
+    Возвращает сумму операции
+    """
+    return f'{transaction["operationAmount"]["amount"]}'
+
+
+def get_currency(transaction: dict[str, dict[str, dict[str, str]]]) -> str:
+    """
+    Возвращает валюту
+    """
+    return f'{transaction["operationAmount"]["currency"]["name"]}'
+
+
+def get_result_transaction(transaction: dict) -> str:
+    """
+    Возвращает информацию об операции
+    :param transaction: dict
+    :return: Возвращает информацию об операции в формате:
+    <дата перевода> <описание перевода>
+    <откуда> -> <куда>
+    <сумма перевода> <валюта>
+    """
+    result = f"{get_format_date(transaction)} {get_description(transaction)}\n" \
+             f"{get_info_from(transaction)} -> {get_info_to(transaction)}\n" \
+             f"{get_summ(transaction)} {get_currency(transaction)}"
+    return result
